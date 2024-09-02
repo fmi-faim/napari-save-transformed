ebac050a4823304da457a8e03b0e50b57375ce3e
Jan Eglinger <jan.eglinger@fmi.ch>
2024-09-02 16:01:29 +0200

    Resave with same dtype, save transforms

--- tests/test_napari_save_transformed.py
+++ tests/test_napari_save_transformed.py
@@ -1,13 +1,14 @@
 import numpy as np
 from napari.layers import Image
+from tifffile.tifffile import imread
 
-from napari_save_transformed import write_transformed_layers
+from napari_save_transformed import TransformDict, write_transformed_layers
 
 
 def test_write_transformed_layers(tmp_path):
     layers = []
     shape = (100, 200)
-    image0 = np.ones(shape=shape)
+    image0 = np.ones(shape=shape, dtype=np.uint16)
     layers.append(Image(image0))
     image1 = image0.copy()
     layers.append(Image(image1, affine=[[1, 0, 30], [0, 1, 10], [0, 0, 1]]))
@@ -16,3 +17,14 @@ def test_write_transformed_layers(tmp_path):
     write_transformed_layers(path=out_path, layer_data=layer_data)
 
     assert out_path.exists()
+
+    output = imread(out_path)
+    assert output.dtype == np.uint16
+    assert output.shape == (2, 130, 210)
+
+    transform_file = out_path.parent / f"{out_path.stem}_transforms.yml"
+    assert transform_file.exists()
+
+    mapping = TransformDict.from_file(transform_file)
+    assert np.array_equal(mapping["image0"], [[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    assert np.array_equal(mapping["image1"], [[1, 0, 30], [0, 1, 10], [0, 0, 1]])
